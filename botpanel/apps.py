3b9f0d5be9562d9867368e256da9330924c0cd27
blackilli <blackilli94@gmail.com>
2025-06-25 00:05:53 +0200

    Update project for coding standards and environment variable usage
    - Replaced hardcoded `SECRET_KEY` with environment variable.
    - Updated string formatting to use double quotes for consistency.
    - Ensured compliance with standard project structure.

--- botpanel/apps.py
+++ botpanel/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BotpanelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'botpanel'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "botpanel"
